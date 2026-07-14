494cf7fcdc54b69c22ae935bfdf7e156cdcb5f2e
KKKdev181 <isaudimadi@gmail.com>
2026-07-14 10:23:19 +0300

    Route technology center tools to working pages

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { LanguageProvider } from "@/contexts/LanguageContext";
 import Index from "./pages/Index.tsx";
+import ToolPage from "./pages/ToolPage.tsx";
 import NotFound from "./pages/NotFound.tsx";
 
 const queryClient = new QueryClient();
@@ -18,6 +19,9 @@ const App = () => (
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
+            <Route path="/tools/project-journey-checklist" element={<ToolPage tool="checklist" />} />
+            <Route path="/tools/request-finder" element={<ToolPage tool="finder" />} />
+            <Route path="/tools/quick-request-match" element={<ToolPage tool="match" />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
