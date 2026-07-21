a0ae320a164bc0541b2a6bb5cfeb4942cbe66623
KKKdev181 <isaudimadi@gmail.com>
2026-07-21 13:16:04 +0000

    Use hash routing for IIS refresh compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -16,7 +16,7 @@ const App = () => (
       <LanguageProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/tools/project-journey-checklist" element={<ProjectJourneyPage />} />
@@ -24,7 +24,7 @@ const App = () => (
             <Route path="/tools/quick-request-match" element={<Navigate replace to="/?tool=quick-request-match" />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </LanguageProvider>
     </TooltipProvider>
   </QueryClientProvider>
