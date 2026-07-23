e221a3da466f1d80e7af4738a29edbf0d623d1d9
KKKdev181 <isaudimadi@gmail.com>
2026-07-23 07:27:12 +0000

    Enable scroll-aware journey navigation

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,6 @@ import App from "./App.tsx";
 import "./index.css";
 import "./styles/project-journey-roadmap.css";
 import "./i18n/legacyToolLocalization";
+import "./utils/projectJourneyScrollSpy";
 
 createRoot(document.getElementById("root")!).render(<App />);
