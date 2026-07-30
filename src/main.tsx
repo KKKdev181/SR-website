25d16dd3c7281611a16d8e7d811b65f47e8f8c93
KKKdev181 <isaudimadi@gmail.com>
2026-07-30 11:23:27 +0000

    Remove Jira iframe script

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,5 @@ import "./styles/project-journey-roadmap.css";
 import "./styles/project-journey-overrides.css";
 import "./i18n/legacyToolLocalization";
 import "./utils/projectJourneyScrollSpy";
-import "./utils/jiraIframe";
 
 createRoot(document.getElementById("root")!).render(<App />);
